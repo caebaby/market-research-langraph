c10de02bab1151ed90b7e13cc02b4a35c213dbe1
caebaby <chris@chrisaevans.com>
2025-07-14 15:22:02 -0400

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,7 @@
+# Create all __init__.py files
+touch core/__init__.py
+touch core/agents/__init__.py
+touch core/memory/__init__.py
+touch teams/__init__.py
+touch teams/icp/__init__.py
+touch teams/icp/agents/__init__.py
