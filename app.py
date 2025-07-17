4434647154fa936b57b70547284b1d274b2f3f75
caebaby <chris@chrisaevans.com>
2025-07-16 20:41:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Form, HTTPException
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 import os
