637034b73e71a62636855a6b1afcfabe1c92319d
caebaby <chris@chrisaevans.com>
2025-07-28 08:22:45 -0400

    Update memory_adapter.py

--- core/memory_adapter.py
+++ core/memory_adapter.py
@@ -1,1 +1,12 @@
+# core/memory_adapter.py
+"""
+Adapter to bridge old and new memory interfaces during Level 4 upgrade
+"""
 
+from typing import List, Dict, Any, Optional
+from datetime import datetime
+
+class MemoryAdapter:
+    """
+    Adapts between old memory interface and new Level 4 interface.
+    """
